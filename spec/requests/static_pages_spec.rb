f85ba7c74b50e894f7c153f45fcbd3e2bced411a
Michael <hrycyniak@wisc.edu>
2014-02-22 14:28:14 -0600

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,60 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
-    end
+describe "Static pages" do
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Madison Bus' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+  describe "FAQ page" do
+    before { visit faq_path }
+
+    let(:heading)    { 'FAQ' }
+    let(:page_title) { 'FAQ' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Blog page" do
+    before { visit blog_path }
+
+    let(:heading)    { 'Blog' }
+    let(:page_title) { 'Blog' }
+
+    it_should_behave_like "all static pages"
+  end
+
+   it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "FAQ"
+    expect(page).to have_title(full_title('FAQ'))
+    click_link "Blog"
+    expect(page).to have_title(full_title('Blog'))
+    click_link "Home"
+    click_link "Madison Bus"
+    expect(page).to have_title(full_title(''))
   end
 end
